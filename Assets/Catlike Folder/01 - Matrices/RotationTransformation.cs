3e7976fb692169dbebfe85a5a33ae852d2060efa
LeventKaya19 <ozgunleventkaya1@gmail.com>
2019-09-06 01:34:31 +0300

    Initial Commit

---
+++ Assets/Catlike Folder/01 - Matrices/RotationTransformation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotationTransformation : Transformation
+{
+
+    public Vector3 rotation;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        return point;
+    }
+}
